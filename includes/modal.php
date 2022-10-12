a6fe126d082a09c9dbb863880575a04f513308d0
Pablo Lires <pablolires@gmail.com>
2022-10-12 12:24:24 -0300

    Modificaciones y arreglos en page tabla surf

---
+++ includes/modal.php
@@ -0,0 +1,19 @@
+<!-- Modal -->
+<div class="modal fade" id="ticketModal" tabindex="-1" aria-labelledby="ticketModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="ticketModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal end -->
--- tabla-ganadores.php
+++ tabla-ganadores.php
@@ -18,26 +18,38 @@
 
 	<img src="./img/commons/fondo.jpg" id="bg" alt="fondo monster">
 
-	<main class="content_tabla">
+	<!-- Modal -->
+	<?php include('includes/modal.php'); ?>
+
+	<main class="content_product">
 		<!-- Header -->
 		<?php include('includes/header.php'); ?>
 
-		<section class="container tabla_surf">
+		<section class="container product tabla_surf">
 
 			<div class="row">
 
 				<div class="col-md-6 content_img">
 					<img class="img-fluid tabla" alt="tabla monster">
+					<div class="data">
+						<img src="./img/commons/flecha.png" class="img-fluid flecha_ganadora" alt="flecha ganadora">
+
+						<a type="button" class="transition" href="#" data-bs-toggle="modal" data-bs-target="#ticketModal">
+							<img src="./img/tabla/conserva.png" class="img-fluid frase_ganadora" alt="conserva el ticket">
+						</a>
+					</div>
 				</div>
 
 				<div class="col-md-6 content_form">
 
 					<div class="frase">
-						<h1>¡FELICITACIONES! GANASTE UNA<br><span class="premio">TABLA DE SURF</span></h1>
-						<img src="./img/commons/subrayado-mobile.png" alt="subrayado">
+						<h1><span class="felicitaciones">¡FELICITACIONES!</span> <br class="first">GANASTE UNA<br><span class="premio">TABLA DE SURF</span></h1>
+						<img class="img-fluid subrayado" src="./img/commons/subrayado-mobile.png" alt="subrayado">
 						<h2>CANJEÁ TU PREMIO</h2>
+						<div class="flecha_abajo">
+							<img class="img-fluid" src="./img/commons/flecha-abajo.png" alt="flecha abajo">
+						</div>
 					</div>
-
 
 					<form id="form-contacto" action="./php/validate-form.php" method="post" class="needs-validation" novalidate>
 
@@ -118,6 +130,12 @@
 			</div>
 
 		</section>
+
+		<div class="col-md-6 content_img_mobile">
+			<a type="button" class="transition" href="#" data-bs-toggle="modal" data-bs-target="#ticketModal">
+				<img src="./img/tabla/tabla-mobile.png" class="img-fluid tabla_mobile" alt="tabla monster mobile">
+			</a>
+		</div>
 
 		<!-- Footer -->
 		<?php include('includes/footer.php'); ?>
